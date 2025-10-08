8725e7ac15f47fa37df9c1b10c120110855018bd
Shahriar Hosssain <shahriar4386@gmail.com>
2025-10-08 12:40:05 +0600

    index.html

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { createBrowserRouter } from "react-router";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+
+
+
+

8f281a3818e4f84e32083c05972a8ec71c64b24b
Shahriar Hosssain <shahriar4386@gmail.com>
2025-10-10 07:56:20 +0600

    Final.html

--- src/Components/ErrorPages/ErrorPages.jsx
+++ src/Components/ErrorPages/ErrorPages.jsx
@@ -6,7 +6,7 @@ const ErrorPages = () => {
     <div className='flex flex-col justify-center items-center h-screen'>
       <img src={errorImg} alt="" />
       <div className='text-center mt-10'>
-        <h1 className='text-6xl font-semibold'>Oops, page not found!</h1>
+        <h1 className='text-6xl font-semibold text-black'>Oops, page not found!</h1>
         <p className='text-xl text-gray-600 mt-5'>The page you are looking for is not available.</p>
       </div>
       <button className="btn bg-linear-to-r from-[#632EE3] to-[#9F62F2] w-40 h-14 text-white mt-5">Go Back!</button>
